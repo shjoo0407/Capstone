ee569c8965eb3ef0af0b5af5bccd8eade3cbf9f1
jeilbitna <zxc715169@gmail.com>
2023-05-13 16:25:27 +0900

    url 수정

--- CapstoneProject/urls.py
+++ CapstoneProject/urls.py
@@ -18,31 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from userinfo.views import UserList
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API 문서",
-        default_version="v1",
-        description="API 문서입니다.",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@contact.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
-
-#path('admin/', admin.site.urls),
-    #path('mydatabase/', UserList.as_view(), name='user-list'),
-    #path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 urlpatterns = [
-    path('fileupload/', include('fileupload.urls')),
     path('admin/', admin.site.urls),
-    path('mydatabase/', include('userinfo.urls')),
-    path('', include('fileupload.urls')),
-    path('account/', include('login.urls')),
+    path('', include('')),
+    path('accounts/', include('accounts.urls')),
+    path('api/', include('api.urls')),
 ]
