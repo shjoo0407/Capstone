c38793049f8d6ef8cede6c2bba2fb30570299c28
rkdtnals974 <mask4117@gmail.com>
2023-05-16 15:51:32 +0900

    Revert "병합 문제"
    This reverts commit bf944d42f09c8d8dd63a54f0d1d90858dce34279, reversing
    changes made to c1a5973afb435911cf5c0f934b290ef1e154adc1.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
@@ -7,30 +9,79 @@ from .models import Account
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 import json
+import jwt
+import logging
+from CapstoneProject.settings import SECRET_KEY
 # Create your views here.
 
-#todo
-# 회원가입(/api/auth/register)
-# def Register(request):
-
-# 로그인(/api/accounts/login)
+# 회원가입
 @csrf_exempt
+def Register(request):
+    if request.method == 'POST':
+        request_data = json.loads(request.body)
+        id = request_data.get('id') # 아이디
+        password = request_data.get('password') # 비밀번호
+        name = request_data.get('name') # 이름
+        birth = request_data.get('birthdate') # 생년월일
+        gender = request_data.get('gender') # 성별
+        height = request_data.get('height') # 키
+        weight = request_data.get('weight') # 몸무게
+
+        account = get_user_model()
+
+        try:
+            # 이미 존재하는 아이디
+            if User.objects.filter(username=id).exists():
+                return JsonResponse({'message': '이미 존재하는 아이디'}, status=400)
+
+            account.objects.create(
+                username=id,
+                password=make_password(password),
+                name=name,
+                birth=birth,
+                gender=gender,
+                height=height,
+                weight=weight
+            )
+            return JsonResponse({'message': '회원가입 성공'}, status=200)
+
+        except Exception as e:
+            return JsonResponse({'message': '오류 발생'}, status=500)
+
+    return JsonResponse({'message': '잘못된 요청'}, status=400)
+
+
+# 로그인(/api/accounts/login) : jwt 토큰 사용
+@csrf_exempt
+#todo 토큰 넘겨주기
 def Login(request):
     if request.method == 'POST':
         request_data = json.loads(request.body)
-        print(f"request : {request}")
-        print(f"request_data : {request_data}")
-        print(f"request body : {request.body}")
+
         username = request_data.get('id')
         password = request_data.get('password')
-        print(f"username : {username}, {type(username)}, password : {password}, {type(password)}")
+
         user = authenticate(request=request, username=username, password=password, user_model=Account) # ID, PW로 확인
 
         # 로그인 성공
         if user is not None:
             print('로그인 성공')
             login(request, user)
+
+            # 토큰 생성
+            expired = datetime.datetime.utcnow() + datetime.timedelta(minutes=5) # 만료 시간
+            payload = {
+                'userid': user.id,
+                'exp': expired,
+            }
+            jwt_token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+            print(f"jwt_token : {jwt_token}")
+            decoded_token = jwt.decode(jwt_token, SECRET_KEY, algorithms=['HS256'])
+            print(f"decoded_token : {decoded_token}")
+            print(decoded_token['userid'], decoded_token['exp'])
+            # 데이터
             data = {
                 'status': '로그인 성공',
                 'id': user.id,
@@ -38,36 +89,71 @@ def Login(request):
                 'birth': user.birth,
                 'gender': user.gender,
                 'height': user.height,
-                'weight': user.weight
+                'weight': user.weight,
+                'token': jwt_token  # JWT 토큰
             }
+            #print(f"data : {data}, type : {type(data)}, 'token : {data['token']}")
             return JsonResponse(data, status=200)
         # 로그인 실패
         else:
             print('로그인 실패')
-            return JsonResponse({'status': '로그인 정보가 틀립니다.'}, status=400)
+            return JsonResponse({'message': '로그인 정보가 틀립니다.'}, status=400)
+
+    # POST 요청이 아닐 때
+    return JsonResponse({'message': '잘못된 요청'}, status=400)
 
 # 로그아웃(/api/accounts/logout)
 def Logout(request):
     logout(request) # 로그아웃
-    return JsonResponse({'status': '로그아웃 성공'}, status=200)
+    return JsonResponse({'message': '로그아웃 성공'}, status=200)
 
 
-# 마이페이지 조회(/api/accounts/mypage/{id})
-@login_required
+#todo 토큰 받아서 사용
+# 마이페이지 조회(/api/accounts/mypage)
 def Mypage(request, id):
-    if request == 'GET':
-        try:
-            user = Account.objects.get(pk=id)
-        except Account.DoesNotExist:
-            return JsonResponse({'status': '해당 계정이 존재하지 않습니다.'}, status=404)
+    if validate_token(request): # 토큰
+        if request == 'GET':
+            try:
+                account = get_user_model() # Account
+                userid = get_id_from_token(request) # 토큰에서 userid 가져옴
+                user = account.objects.get(id=userid)
+            except account.DoesNotExist:
+                return JsonResponse({'message': '해당 계정이 존재하지 않습니다.'}, status=404)
 
-        data = {
-            'status': '마이페이지 조회 성공',
-            'id': user.id,
-            'name': user.name,
-            'birth': user.birth,
-            'gender': user.gender,
-            'height': user.height,
-            'weight': user.weight
-        }
-        return JsonResponse(data, status=200)
+            data = {
+                'message': '마이페이지 조회 성공',
+                'id': user.username,
+                'name': user.name,
+                'birth': user.birth,
+                'gender': user.gender,
+                'height': user.height,
+                'weight': user.weight
+            }
+            return JsonResponse(data, status=200)
+
+# (jwt)토큰 유효성 검증
+def validate_token(request):
+    jwt_token = request.META.get('HTTP_AUTHORIZATION').split(' ')[1] #todo (지금은 Header의 'HTTP_AUTHORIZATION'에 ['Bearer', '<token>'] 요청받는 경우)
+    try:
+        decoded_token = jwt.decode(jwt_token, SECRET_KEY, algorithms=['HS256']) #todo
+        userid = decoded_token['userid']
+
+        account = get_user_model()
+        user = account.objects.get(id=userid)
+
+        if user.is_authenticated:
+            return True # 사용자 인증 성공
+        else:
+            return False # 사용자 인증 실패
+
+    except jwt.ExpiredSignatureError:
+        return False # 토큰 만료
+    except (jwt.InvalidTokenError, User.DoesNotExist):
+        return False # 유효하지 않은 토큰
+
+# (jwt) 토큰에서 사용자 id 받아오기(실제 아이디는 username)
+def get_id_from_token(request):
+    jwt_token = request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
+    decoded_token = jwt.decode(jwt_token, SECRET_KEY, algorithms=['HS256'])
+    userid = decoded_token['userid']
+    return userid
