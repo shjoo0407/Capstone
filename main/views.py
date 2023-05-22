05c097cba852eea7b12824e4dffcc58f05c12c89
rkdtnals974 <mask4117@gmail.com>
2023-05-22 18:02:53 +0900

    230522 views.py의 Statistics 구현

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 import json
 from django.http import JsonResponse
 from .models import Gallery
-
+from collections import defaultdict
 
 # Create your views here.
 # todo 식단 업로드 페이지 조회
@@ -14,7 +14,21 @@ def Upload(request):
         if validate_token(request):
             userid = get_id_from_token(request)
 
+            # 모든 Gallery 객체를 조회합니다.
+            galleries = Gallery.objects.all()
+            # 각 객체의 정보를 JSON 형식으로 변환합니다.
+            data = [{'name': gallery.name,
+                     'total': gallery.total,
+                     'kcal': gallery.kcal,
+                     'protein': gallery.protein,
+                     'carbon': gallery.carbon,
+                     'fat': gallery.fat,
+                     'uploaded_at': gallery.uploaded_at} for gallery in galleries]
+            # 결과를 반환합니다.
+            return JsonResponse(data, safe=False, status=200)
+
         return
+
     if validate_token(request):
         if request.method == "POST":
             #request_data = json.loads(request.body)
@@ -50,10 +64,42 @@ def Daily(request):
 
 # todo 식단 통계 페이지 조회
 def Statistics(request):
+    data = {}
     if request.method == 'GET':
+
         if validate_token(request):
+
             userid = get_id_from_token(request)
-        return
+
+            galleries = Gallery.objects.all()
+
+            aggregated_data={
+                'pro': defaultdict(int),
+                'carbon': defaultdict(int),
+                'fat': defaultdict(int)
+            }
+
+            for gallery in galleries:
+                date = gallery.uploaded_at.strftime("%Y-%m-%d")
+                aggregated_data['pro'][date] +=  gallery.pro
+                aggregated_data['carbon'][date] += gallery.carbon
+                aggregated_data['fat'][date] += gallery.fat
+
+            data = {
+                'pro': [],
+                'carbon': [],
+                'fat':[],
+            }
+
+            for date, pro in aggregated_data['pro'].items():
+                data['pro'].append({'x': date, 'y': pro})
+            for date, carbon in aggregated_data['carbon'].items():
+                data['carbon'].append({'x': date, 'y': carbon})
+            for date, fat in aggregated_data['fat'].items():
+                data['fat'].append({'x': date, 'y': fat})
+
+        return JsonResponse(data, safe=False)
+
     return JsonResponse({'message': '잘못된 요청'}, status=500)
 
 
